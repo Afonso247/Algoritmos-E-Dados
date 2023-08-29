e046ffbb64f437aa13bfdccc98dae3f9529cb31f
Afonso247 <afonso.h2002@gmail.com>
2023-08-29 18:59:38 -0300

    Primeiro commit

---
+++ data.py
@@ -0,0 +1,79 @@
+
+arrayList = [
+    [
+        # Janeiro
+        [
+            {
+                'ID': 0,
+                'nome_do_produto': 'Coca-Cola',
+                'tipo_do_produto': 'Bebidas',
+                'preco_do_produto': 10.89,
+                'nome_do_comprador': 'João',
+                'data_de_compra': '02-01-2023'
+            },
+            {
+                'ID': 1,
+                'nome_do_produto': 'Frango Sadia',
+                'tipo_do_produto': 'Produtos Frescos',
+                'preco_do_produto': 13.99,
+                'nome_do_comprador': 'Daniel',
+                'data_de_compra': '06-01-2023'
+            },
+            {
+                'ID': 2,
+                'nome_do_produto': 'Ketchup Heinz',
+                'tipo_do_produto': 'Condimentos',
+                'preco_do_produto': 16.49,
+                'nome_do_comprador': 'Malaquias',
+                'data_de_compra': '21-01-2023'
+            }
+        ],
+        # Fevereiro
+        [
+            {
+                'ID': 3,
+                'nome_do_produto': 'Doritos',
+                'tipo_do_produto': 'Salgados',
+                'preco_do_produto': 5.49,
+                'nome_do_comprador': 'Pedro',
+                'data_de_compra': '04-02-2023'
+            },
+            {
+                'ID': 4,
+                'nome_do_produto': 'Guaraná Antarctica',
+                'tipo_do_produto': 'Bebidas',
+                'preco_do_produto': 8.99,
+                'nome_do_comprador': 'Saulo',
+                'data_de_compra': '16-02-2023'
+            },
+            {
+                'ID': 5,
+                'nome_do_produto': 'Feijão Macassar',
+                'tipo_do_produto': 'Grãos',
+                'preco_do_produto': 10.49,
+                'nome_do_comprador': 'Paulo',
+                'data_de_compra': '27-02-2023'
+            }
+        ],
+        # Março
+        [
+                        
+        ],
+        # Abril
+        [
+            
+        ],
+        # Maio
+        [
+            
+        ],
+        # Junho
+        [
+            
+        ],
+        # Julho
+        [
+            
+        ]
+    ]
+]
---
+++ main.py
@@ -0,0 +1,6 @@
+# Importe os dados do arquivo dados.py
+from data import arrayList
+
+# Agora você pode usar as variáveis importadas
+print(arrayList[0][0][0]['nome_do_produto'])
+
