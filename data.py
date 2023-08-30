9adf18ba9220583d6335156c5d44db55db5ba6cc
Suelen Salvino <suelensalvinocs@gmail.com>
2023-08-30 19:03:43 -0300

    Add array de dados

--- data.py
+++ data.py
@@ -1,83 +1,61 @@
-# Import do datetime, para facilitar a consulta da data
-# dt(ano, mês, dia)
-from datetime import datetime as dt
+# Definindo os nomes das filiais, meses e tipos de produtos
+filiais = ['PE', 'PB', 'CE']
+meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul']
+tipos_de_produtos = ['hortfruit', 'bebidas', 'lacticinios']
 
-
-arrayList = [
+# Criando o array tridimensional com valores
+vendas = [
+    # Filial PE
     [
         # Janeiro
-        [
-            {
-                'ID': 0,
-                'nome_do_produto': 'Coca-Cola',
-                'tipo_do_produto': 'Bebidas',
-                'preco_do_produto': 10.89,
-                'nome_do_comprador': 'João',
-                'data_de_compra': dt(2023, 1, 2)
-            },
-            {
-                'ID': 1,
-                'nome_do_produto': 'Frango Sadia',
-                'tipo_do_produto': 'Produtos Frescos',
-                'preco_do_produto': 13.99,
-                'nome_do_comprador': 'Daniel',
-                'data_de_compra': dt(2023, 1, 9)
-            },
-            {
-                'ID': 2,
-                'nome_do_produto': 'Ketchup Heinz',
-                'tipo_do_produto': 'Condimentos',
-                'preco_do_produto': 16.49,
-                'nome_do_comprador': 'Malaquias',
-                'data_de_compra': dt(2023, 1, 23)
-            }
-        ],
+        [15000, 10000, 12000],  # hortfruit, bebidas, lacticinios
         # Fevereiro
-        [
-            {
-                'ID': 3,
-                'nome_do_produto': 'Doritos',
-                'tipo_do_produto': 'Salgados',
-                'preco_do_produto': 5.49,
-                'nome_do_comprador': 'Pedro',
-                'data_de_compra': dt(2023, 2, 4)
-            },
-            {
-                'ID': 4,
-                'nome_do_produto': 'Guaraná Antarctica',
-                'tipo_do_produto': 'Bebidas',
-                'preco_do_produto': 8.99,
-                'nome_do_comprador': 'Saulo',
-                'data_de_compra': dt(2023, 2, 16)
-            },
-            {
-                'ID': 5,
-                'nome_do_produto': 'Feijão Macassar',
-                'tipo_do_produto': 'Grãos',
-                'preco_do_produto': 10.49,
-                'nome_do_comprador': 'Paulo',
-                'data_de_compra': dt(2023, 2, 27)
-            }
-        ],
+        [13000, 11000, 11500],
         # Março
-        [
-                        
-        ],
+        [16000, 10500, 12500],
         # Abril
-        [
-            
-        ],
+        [14000, 12000, 13000],
         # Maio
-        [
-            
-        ],
+        [15500, 11500, 12800],
         # Junho
-        [
-            
-        ],
+        [14500, 10500, 13200],
         # Julho
-        [
-            
-        ]
+        [15000, 10000, 12000]
+    ],
+
+    # Filial PB
+    [
+        # Janeiro
+        [12000, 9500, 11000],
+        # Fevereiro
+        [11000, 9000, 10500],
+        # Março
+        [12500, 10000, 12000],
+        # Abril
+        [13000, 10500, 12500],
+        # Maio
+        [11500, 9500, 11500],
+        # Junho
+        [11800, 9200, 11000],
+        # Julho
+        [12000, 9800, 11200]
+    ],
+
+    # Filial CE
+    [
+        # Janeiro
+        [17000, 13000, 14000],
+        # Fevereiro
+        [18000, 13500, 15000],
+        # Março
+        [16000, 12500, 13500],
+        # Abril
+        [17500, 13000, 14200],
+        # Maio
+        [16500, 12800, 13800],
+        # Junho
+        [16800, 13200, 14200],
+        # Julho
+        [17000, 13000, 14100]
     ]
 ]
