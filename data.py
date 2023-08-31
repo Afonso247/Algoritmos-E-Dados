669086a03124bec30656af660b5138ff282afd24
Suelen Salvino <suelensalvinocs@gmail.com>
2023-08-31 17:20:22 -0300

    Transformando dados em objetos

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 # Definindo os nomes das filiais, meses e tipos de produtos
 filiais = ['PE', 'PB', 'CE']
-meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul']
+meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun']
 tipos_de_produtos = ['hortifruit', 'bebidas', 'lacticinios']
 
 # Criando o array tridimensional com valores
@@ -8,25 +8,26 @@ vendas = [
     # Filial PE
     [
         # Janeiro
-            # hortifruit, bebidas, e lacticinios, nesta ordem
+        # hortifruit, bebidas, e lacticinios, nesta ordem
         [
             {
                 'ID': 0,
                 'tipo_do_produto': "hortifruit",
-                'quantidade_vendida': 1250,
-                'valor_total': 15000
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 130
             },
             {
                 'ID': 1,
                 'tipo_do_produto': "bebidas",
-                'quantidade_vendida': 1020,
-                'valor_total': 10000
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1020
             },
             {
                 'ID': 2,
                 'tipo_do_produto': "lacticinios",
-                'quantidade_vendida': 1450,
-                'valor_total': 12000
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 1450
+                
             }
         ],
         # Fevereiro
@@ -34,20 +35,20 @@ vendas = [
             {
                 'ID': 3,
                 'tipo_do_produto': "hortifruit",
-                'quantidade_vendida': 1190,
-                'valor_total': 13000
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1190
             },
             {
                 'ID': 4,
                 'tipo_do_produto': "bebidas",
-                'quantidade_vendida': 1000,
-                'valor_total': 11000
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1000
             },
             {
                 'ID': 5,
                 'tipo_do_produto': "lacticinios",
-                'quantidade_vendida': 960,
-                'valor_total': 11500
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 960
             }
         ],
         # Março
@@ -55,59 +56,344 @@ vendas = [
             {
                 'ID': 6,
                 'tipo_do_produto': "hortifruit",
-                'quantidade_vendida': 1300,
-                'valor_total': 16000
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1300
             },
             {
                 'ID': 7,
                 'tipo_do_produto': "bebidas",
-                'quantidade_vendida': 1080,
-                'valor_total': 10500
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1080
             },
             {
                 'ID': 8,
                 'tipo_do_produto': "lacticinios",
-                'quantidade_vendida': 1420,
-                'valor_total': 12500
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 1420
             }
         ],
         # Abril
-        [14000, 12000, 13000],
+        [
+            {
+                'ID': 9,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1050
+            },
+            {
+                'ID': 10,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1000
+            },
+            {
+                'ID': 11,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 450
+            }
+        ],
         # Maio
-        [15500, 11500, 12800],
+        [ 
+            {
+                'ID': 12,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 500
+            },
+            {
+                'ID': 13,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 930
+            },
+            {
+                'ID': 14,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 350
+            }
+        ],
         # Junho
-        [14500, 10500, 13200]
+        [   
+            {
+                'ID': 15,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1100
+            },
+            {
+                'ID': 16,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1005
+            },
+            {
+                'ID': 17,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 500
+            }
+        ]
     ],
 
     # Filial PB
     [
         # Janeiro
-        [12000, 9500, 11000],
+        [
+            {
+                'ID': 18,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1050
+            },
+            {
+                'ID': 19,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1000
+            },
+            {
+                'ID': 20,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 450
+            }            
+        ],
         # Fevereiro
-        [11000, 9000, 10500],
+        [
+            {
+                'ID': 21,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1130
+            },
+            {
+                'ID': 22,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1020
+            },
+            {
+                'ID': 23,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 550
+            }
+        ],
         # Março
-        [12500, 10000, 12000],
+        [
+            {
+                'ID': 24,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1000
+            },
+            {
+                'ID': 25,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1200
+            },
+            {
+                'ID': 26,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 700
+            }
+        ],
         # Abril
-        [13000, 10500, 12500],
+        [
+            {
+                'ID': 27,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1350
+            },
+            {
+                'ID': 28,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1530
+            },
+            {
+                'ID': 29,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 850
+            }
+        ],
         # Maio
-        [11500, 9500, 11500],
+        [
+            {
+                'ID': 30,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1200
+            },
+            {
+                'ID': 31,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1040
+            },
+            {
+                'ID': 32,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 750
+            }
+        ],
         # Junho
-        [11800, 9200, 11000]
+        [
+            {
+                'ID': 33,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1500
+            },
+            {
+                'ID': 34,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 990
+            },
+            {
+                'ID': 35,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 550
+            }
+        ]
     ],
 
     # Filial CE
     [
         # Janeiro
-        [17000, 13000, 14000],
+        [
+            {
+                'ID': 36,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1150
+            },
+            {
+                'ID': 37,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1005
+            },
+            {
+                'ID': 38,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 850
+            }
+        ],
         # Fevereiro
-        [18000, 13500, 15000],
+        [
+            {
+                'ID': 39,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1250
+            },
+            {
+                'ID': 40,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1055
+            },
+            {
+                'ID': 41,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 367
+            }
+        ],
         # Março
-        [16000, 12500, 13500],
+        [
+            {
+                'ID': 42,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1000
+            },
+            {
+                'ID': 43,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 920
+            },
+            {
+                'ID': 44,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 720
+            }
+        ],
         # Abril
-        [17500, 13000, 14200],
+        [
+            {
+                'ID': 45,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1350
+            },
+            {
+                'ID': 46,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1130
+            },
+            {
+                'ID': 47,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 780
+            }
+        ],
         # Maio
-        [16500, 12800, 13800],
+        [
+            {
+                'ID': 48,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1230
+            },
+            {
+                'ID': 49,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1510
+            },
+            {
+                'ID': 50,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 900
+            }
+        ],
         # Junho
-        [16800, 13200, 14200]
+        [
+            {
+                'ID': 51,
+                'tipo_do_produto': "hortifruit",
+                'preco_do_produto': 10.99,
+                'quantidade_vendida': 1000
+            },
+            {
+                'ID': 52,
+                'tipo_do_produto': "bebidas",
+                'preco_do_produto': 24.99,
+                'quantidade_vendida': 1050
+            },
+            {
+                'ID': 53,
+                'tipo_do_produto': "lacticinios",
+                'preco_do_produto': 10.49,
+                'quantidade_vendida': 650
+            }
+        ]
     ]
 ]
