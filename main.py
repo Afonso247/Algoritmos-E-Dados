1205d4d34e9b0f362b7f53879b178fc77ee386dc
Afonso Henrique <97695756+Afonso247@users.noreply.github.com>
2023-09-02 17:46:15 -0300

    Atualização do terminal de menu

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import time
-from script import (
+from scriptgeral import (
     findTotalQuantia_geral as fTQg,
     findMediaQuantia_geral as fMQg,
     findTotalRenda_geral as fTRg,
     findMediaRenda_geral as fMRg
 )
+from scriptpormes import (
+    findTotalQuantia_porMes as fTQpM,
+    findTotalRenda_porMes as fTRpM,
+    findMediaQuantia_porMes as fMQpM,
+    findMediaRenda_porMes as fMRpM
+)
 
 print("Bem-vindo a rede de supermercados")
-print("-----")
 
+
+# Início da interface
 def start():
     
-
-    print("Como você deseja consultar?")
-    print("Por Quantia: 1")
-    print("Por Renda: 2")
+    
+    print("Em que formato você deseja consultar?")
+    print("Por valor total: 1")
+    print("Por valor mediano: 2")
     print("Sair: 0")
 
     select = input("Digite aqui: ")
@@ -22,9 +29,11 @@ def start():
     
     try:
         if(int(select) == 1):
-            return quantiaSel()
+            print("-----")
+            return totalSel()
         elif(int(select) == 2):
-            return rendaSel()
+            print("-----")
+            return mediaSel()
         elif(int(select) == 0):
             print("Até mais!")
             return None
@@ -39,10 +48,11 @@ def start():
         time.sleep(1)
         return start()
 
-def quantiaSel():
-    print("De que forma (Quantia)?")
-    print("Por valor total: 1")
-    print("Por valor mediano: 2")
+# Seleção Total
+def totalSel():
+    print("Selecione o tipo da consulta... (Total)")
+    print("Por Quantia: 1")
+    print("Por Renda: 2")
     print("Voltar: 0")
     
     select = input("Digite aqui: ")
@@ -50,43 +60,181 @@ def quantiaSel():
     
     try:
         if(int(select) == 1):
+            print("-----")
+            return quantiaTotalSel()
+        elif(int(select) == 2):
+            print("-----")
+            return rendaTotalSel()
+        elif(int(select) == 0):
+            print("Voltando...")
+            print("-----")
+            time.sleep(1)
+            return start()
+        else:
+            print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+            print("-----")
+            time.sleep(1)
+            return totalSel()
+    except ValueError:
+        print("Houve um erro de valor! Tente novamente.")
+        print("-----")
+        time.sleep(1)
+        return totalSel()
+    
+def quantiaTotalSel():
+    print("Selecione o mês de início a ser consultado... (Quantia - Total)")
+    print("Jan-Jun: 1-6")
+    print("Consulta geral: 7")
+    print("Voltar: 0")
+    
+    select = input("Digite aqui: ")
+    
+    
+    try:
+        if(1 <= int(select) <= 6):
+            print("Selecione o mes final a ser consultado... (Quantia - Total)")
+            print("Jan-Jun: 1-6")
+            print("Consulta geral: 7")
+            print("Voltar: 0")
+            
+            selector = input("Digite aqui: ")
+            
+            if(int(selector) != 0 and int(selector) < int(select)):
+                print("Mês final é menor do que o mês inicial. Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return quantiaTotalSel()
+            
+            try:
+                if(1 <= int(selector) <= 6):
+                    fTQpM(int(select)-1, int(selector))
+                    print("-----")
+                    return start()
+                elif(int(selector) == 7):
+                    fTQg()
+                    print("-----")
+                    return start()
+                elif(int(selector) == 0):
+                    print("Voltando...")
+                    print("-----")
+                    time.sleep(1)
+                    return quantiaTotalSel()
+                else:
+                    print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+                    print("-----")
+                    time.sleep(1)
+                    return quantiaTotalSel()
+            except ValueError:
+                print("Houve um erro de valor! Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return quantiaTotalSel()
+        elif(int(select) == 7):
             fTQg()
-            return start()
-        elif(int(select) == 2):
-            fMQg()
+            print("-----")
             return start()
         elif(int(select) == 0):
             print("Voltando...")
             print("-----")
             time.sleep(1)
-            return start()
+            return totalSel()
         else:
             print("Houve um erro! Provavelmente você digitou uma opção inválida...")
             print("-----")
             time.sleep(1)
-            return quantiaSel()
+            return quantiaTotalSel()
     except ValueError:
         print("Houve um erro de valor! Tente novamente.")
         print("-----")
         time.sleep(1)
-        return quantiaSel()
+        return quantiaTotalSel()
     
-def rendaSel():
-    print("De que forma (Renda)?")
-    print("Por valor total: 1")
-    print("Por valor mediano: 2")
+def rendaTotalSel():
+    print("Selecione o mês de início a ser consultado... (Renda - Total)")
+    print("Jan-Jun: 1-6")
+    print("Consulta geral: 7")
     print("Voltar: 0")
     
     select = input("Digite aqui: ")
+    
+    
+    try:
+        if(1 <= int(select) <= 6):
+            print("Selecione o mes final a ser consultado... (Renda - Total)")
+            print("Jan-Jun: 1-6")
+            print("Consulta geral: 7")
+            print("Voltar: 0")
+            
+            selector = input("Digite aqui: ")
+            
+            if(int(selector) != 0 and int(selector) < int(select)):
+                print("Mês final é menor do que o mês inicial. Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return rendaTotalSel()
+            
+            try:
+                if(1 <= int(selector) <= 6):
+                    fTRpM(int(select)-1, int(selector))
+                    print("-----")
+                    return start()
+                elif(int(selector) == 7):
+                    fTRg()
+                    print("-----")
+                    return start()
+                elif(int(selector) == 0):
+                    print("Voltando...")
+                    print("-----")
+                    time.sleep(1)
+                    return rendaTotalSel()
+                else:
+                    print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+                    print("-----")
+                    time.sleep(1)
+                    return rendaTotalSel()
+            except ValueError:
+                print("Houve um erro de valor! Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return rendaTotalSel()
+        elif(int(select) == 7):
+            fTRg()
+            print("-----")
+            return start()
+        elif(int(select) == 0):
+            print("Voltando...")
+            print("-----")
+            time.sleep(1)
+            return totalSel()
+        else:
+            print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+            print("-----")
+            time.sleep(1)
+            return rendaTotalSel()
+    except ValueError:
+        print("Houve um erro de valor! Tente novamente.")
+        print("-----")
+        time.sleep(1)
+        return rendaTotalSel()
 
+
+# Seleção Média
+def mediaSel():
+    print("Selecione o tipo da consulta... (Média)")
+    print("Por Quantia: 1")
+    print("Por Renda: 2")
+    print("Voltar: 0")
+    
+    select = input("Digite aqui: ")
+    
     
     try:
         if(int(select) == 1):
-            fTRg()
-            return start()
+            print("-----")
+            return quantiaMediaSel()
         elif(int(select) == 2):
-            fMRg()
-            return start()
+            print("-----")
+            return rendaMediaSel()
         elif(int(select) == 0):
             print("Voltando...")
             print("-----")
@@ -96,13 +244,148 @@ def rendaSel():
             print("Houve um erro! Provavelmente você digitou uma opção inválida...")
             print("-----")
             time.sleep(1)
-            return rendaSel()
+            return mediaSel()
     except ValueError:
         print("Houve um erro de valor! Tente novamente.")
         print("-----")
         time.sleep(1)
-        return rendaSel()
+        return mediaSel()
     
+def quantiaMediaSel():
+    print("Selecione o mês de início a ser consultado... (Quantia - Media)")
+    print("Jan-Jun: 1-6")
+    print("Consulta geral: 7")
+    print("Voltar: 0")
+    
+    select = input("Digite aqui: ")
+    
+    
+    try:
+        if(1 <= int(select) <= 6):
+            print("Selecione o mes final a ser consultado... (Quantia - Media)")
+            print("Jan-Jun: 1-6")
+            print("Consulta geral: 7")
+            print("Voltar: 0")
+            
+            selector = input("Digite aqui: ")
+            
+            if(int(selector) != 0 and int(selector) < int(select)):
+                print("Mês final é menor do que o mês inicial. Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return quantiaMediaSel()
+            
+            try:
+                if(1 <= int(selector) <= 6):
+                    fMQpM(int(select)-1, int(selector))
+                    print("-----")
+                    return start()
+                elif(int(selector) == 7):
+                    fMQg()
+                    print("-----")
+                    return start()
+                elif(int(selector) == 0):
+                    print("Voltando...")
+                    print("-----")
+                    time.sleep(1)
+                    return quantiaMediaSel()
+                else:
+                    print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+                    print("-----")
+                    time.sleep(1)
+                    return quantiaMediaSel()
+            except ValueError:
+                print("Houve um erro de valor! Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return quantiaMediaSel()
+        elif(int(select) == 7):
+            fMQg()
+            print("-----")
+            return start()
+        elif(int(select) == 0):
+            print("Voltando...")
+            print("-----")
+            time.sleep(1)
+            return mediaSel()
+        else:
+            print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+            print("-----")
+            time.sleep(1)
+            return quantiaMediaSel()
+    except ValueError:
+        print("Houve um erro de valor! Tente novamente.")
+        print("-----")
+        time.sleep(1)
+        return quantiaMediaSel()
+    
+def rendaMediaSel():
+    print("Selecione o mês de início a ser consultado... (Renda - Media)")
+    print("Jan-Jun: 1-6")
+    print("Consulta geral: 7")
+    print("Voltar: 0")
+    
+    select = input("Digite aqui: ")
+    
+    
+    try:
+        if(1 <= int(select) <= 6):
+            print("Selecione o mes final a ser consultado... (Renda - Media)")
+            print("Jan-Jun: 1-6")
+            print("Consulta geral: 7")
+            print("Voltar: 0")
+            
+            selector = input("Digite aqui: ")
+            
+            if(int(selector) != 0 and int(selector) < int(select)):
+                print("Mês final é menor do que o mês inicial. Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return rendaMediaSel()
+            
+            try:
+                if(1 <= int(selector) <= 6):
+                    fMRpM(int(select)-1, int(selector))
+                    print("-----")
+                    return start()
+                elif(int(selector) == 7):
+                    fMRg()
+                    print("-----")
+                    return start()
+                elif(int(selector) == 0):
+                    print("Voltando...")
+                    print("-----")
+                    time.sleep(1)
+                    return rendaMediaSel()
+                else:
+                    print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+                    print("-----")
+                    time.sleep(1)
+                    return rendaMediaSel()
+            except ValueError:
+                print("Houve um erro de valor! Tente novamente.")
+                print("-----")
+                time.sleep(1)
+                return rendaMediaSel()
+        elif(int(select) == 7):
+            fMRg()
+            print("-----")
+            return start()
+        elif(int(select) == 0):
+            print("Voltando...")
+            print("-----")
+            time.sleep(1)
+            return mediaSel()
+        else:
+            print("Houve um erro! Provavelmente você digitou uma opção inválida...")
+            print("-----")
+            time.sleep(1)
+            return rendaMediaSel()
+    except ValueError:
+        print("Houve um erro de valor! Tente novamente.")
+        print("-----")
+        time.sleep(1)
+        return rendaMediaSel()
 
 
 
